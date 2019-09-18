8e093bf5cd68fb1f35f84b8d24c26835ba5ea0ce
unknown <jai00271@gmail.com>
2019-09-18 05:42:28 +0530

    modified init file

---
+++ Extentions/__init__.py
@@ -0,0 +1,1 @@
+from .TFRecordHelper import *
--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
-from .1. Basics of CNN import *
-from .2. Neural Architecture import *
-from .3. Our First Neural Network import *
-from .4. Architectural Basics import *
-from .5. Batch Normalization & Regularization import *
-from .6. Advanced Convolutions import *
-from .7. Network Architectures & Receptive Fields import *
-from .9. Data Augmentation import *
-from .10. Visualizations import *
-from .11. Learning Rate import *
-from .12 13 14. RESNET PART II import *
-from .15 16. Inception DENSENETs import *
+# from .1. Basics of CNN import *
+# from .2. Neural Architecture import *
+# from .3. Our First Neural Network import *
+# from .4. Architectural Basics import *
+# from .5. Batch Normalization & Regularization import *
+# from .6. Advanced Convolutions import *
+# from .7. Network Architectures & Receptive Fields import *
+# from .9. Data Augmentation import *
+# from .10. Visualizations import *
+# from .11. Learning Rate import *
+# from .12 13 14. RESNET PART II import *
+# from .15 16. Inception DENSENETs import *
 from .Extentions import *
